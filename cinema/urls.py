61447deefdc5ca42d583e12b36daf1cfcc26a869
mykyta <nikita.fedotov222@gmail.com>
2024-09-10 16:17:34 +0300

    solution

--- cinema/urls.py
+++ cinema/urls.py
@@ -12,19 +12,18 @@ from cinema.views import (
 app_name = "cinema"
 
 router = routers.DefaultRouter()
+router.register("movies", MovieViewSet, basename="movies")
+router.register("genres", GenreViewSet, basename="genres")
+router.register(
+    "cinema_halls",
+    CinemaHallViewSet,
+    basename="cinema_halls"
+)
+router.register(
+    "movie_sessions",
+    MovieSessionViewSet,
+    basename="movie-sessions"
+)
+router.register("actors", ActorViewSet, basename="actors")
 
-urlpatterns = [
-    path(router.register("movies", MovieViewSet, basename="movies")),
-    path(router.register("genres", GenreViewSet, basename="genres")),
-    path(router.register(
-        "cinema_halls",
-        CinemaHallViewSet,
-        basename="cinema-halls"
-    )),
-    path(router.register(
-        "movie_sessions",
-        MovieSessionViewSet,
-        basename="movie-sessions"
-    )),
-    path(router.register("actors", ActorViewSet, basename="actors")),
-]
+urlpatterns = router.urls
